cbd4ceed8999c27c1663b2590071aadb2950334c
Yeray <yeray3000@gmail.com>
2019-10-01 08:03:49 +0200

    BmiCalculator done, xml view done

--- app/src/main/java/es/iessaladillo/pedrojoya/pr01/bmi/BmiCalculator.java
+++ app/src/main/java/es/iessaladillo/pedrojoya/pr01/bmi/BmiCalculator.java
@@ -6,12 +6,13 @@ package es.iessaladillo.pedrojoya.pr01.bmi;
 public class BmiCalculator {
 
     /**
-     * @param weightInKgs Weight of the person in kgs
+     * @param weightInKgs    Weight of the person in kgs
      * @param heightInMeters Height of the person in meters
      * @return The body mass index (BMI)
      */
-    public float calculateBmi(float weightInKgs, float heightInMeters) {
-        // TODO
+    public static float calculateBmi(float weightInKgs, float heightInMeters) {
+        float bmi = weightInKgs / (heightInMeters * heightInMeters);
+        return bmi;
     }
 
 
@@ -19,8 +20,15 @@ public class BmiCalculator {
      * @param bmi Body mass index to get clasification from
      * @return A BmiClasification enum with the clasification of BMI
      */
-    public BmiClasification getBmiClasification(float bmi) {
-        // TODO
+    public static BmiClasification getBmiClasification(float bmi) {
+        BmiClasification result = null;
+        if (bmi < 18.5f) result = BmiClasification.LOW_WEIGHT;
+        if (18.5f < bmi && bmi < 24.9f) result = BmiClasification.NORMAL_WEIGHT;
+        if (24.9f < bmi && bmi < 29.9f) result = BmiClasification.OVERWWEIGHT;
+        if (29.9f < bmi && bmi < 34.9f) result = BmiClasification.OBESITY_GRADE_1;
+        if (34.9f < bmi && bmi < 39.9f) result = BmiClasification.OBESITY_GRADE_2;
+        if (bmi > 39.9f) result = BmiClasification.OBESITY_GRADE_3;
+        return result;
     }
 
     public enum BmiClasification {
--- app/src/main/java/es/iessaladillo/pedrojoya/pr01/ui/main/MainActivity.java
+++ app/src/main/java/es/iessaladillo/pedrojoya/pr01/ui/main/MainActivity.java
@@ -1,19 +1,86 @@
 package es.iessaladillo.pedrojoya.pr01.ui.main;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
 import es.iessaladillo.pedrojoya.pr01.R;
+import es.iessaladillo.pedrojoya.pr01.bmi.BmiCalculator;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtWeight;
+    private EditText txtHeight;
+    private TextView lblResult;
+    private Button btnReset;
+    private Button btnCalculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-        // TODO
+        setViews();
+        setButtons();
     }
 
-    // TODO
+    private void setButtons() {
+        btnCalculate.setOnClickListener(v -> calculateBmi(Float.parseFloat(txtWeight.getText().toString()),Float.parseFloat(txtHeight.getText().toString())));
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetFields();
+                resetImg();
+                resetResult();
+            }
+        });
+    }
+
+    private void resetResult() {
+        //TODO resetear campo de lblResult.
+    }
+
+    private void resetImg() {
+        //TODO resetear img a inicial.
+    }
+
+    private void resetFields() {
+        //TODO resetear campos de height y weight.
+    }
+
+    private void calculateBmi(float weight, float height) {
+        //TODO Cambio de imagen y lblResult segun resultado.
+        float result;
+        result = BmiCalculator.calculateBmi(weight,height);
+
+        switch (BmiCalculator.getBmiClasification(result)){
+
+            case LOW_WEIGHT:
+                break;
+            case NORMAL_WEIGHT:
+                break;
+            case OVERWWEIGHT:
+                break;
+            case OBESITY_GRADE_1:
+                break;
+            case OBESITY_GRADE_2:
+                break;
+            case OBESITY_GRADE_3:
+                break;
+        }
+    }
+
+    private void setViews() {
+        btnCalculate = ActivityCompat.requireViewById(this, R.id.btnCalculate);
+        btnReset = ActivityCompat.requireViewById(this, R.id.btnReset);
+        lblResult = ActivityCompat.requireViewById(this, R.id.lblResult);
+        txtHeight = ActivityCompat.requireViewById(this, R.id.txtHeight);
+        txtWeight = ActivityCompat.requireViewById(this, R.id.txtWeight);
+    }
+
 
 }
